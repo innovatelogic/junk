e41168d9e16a2f60abcc8a3188c474dd833493fc
YurG <yura.gunko@gmail.com>
2016-10-31 01:59:54 +0200

    compare_binary_hex_digit

---
+++ src/ut/ut_compare_binary_hex_digit.cpp
@@ -0,0 +1,13 @@
+#include "../compare_binary_hex_digit.h"
+#include "gtest/gtest.h"
+
+namespace junk
+{
+
+    TEST(Numbers, StrHEX_vs_Bin)
+    {
+        int i = numbers::str_to_int("100", 2);
+
+        EXPECT_TRUE(numbers::compare_hex_bin("3F3", "001111110011"));
+    }
+}
