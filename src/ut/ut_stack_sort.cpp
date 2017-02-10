a6fed512ec176499257ee9a3f924b707aa32af08
yurg <westgames@gmail.com>
2017-02-10 13:53:18 +0200

    stack sort

--- src/ut/ut_stack_sort.cpp
+++ src/ut/ut_stack_sort.cpp
@@ -6,24 +6,32 @@ namespace junk
 {
     namespace stack_sort
     {
+        int compare(const void * a, const void * b)
+        {
+            return (*(int*)a - *(int*)b);
+        }
+
         TEST(Stack, StackSort)
         {
             std::stack<int> stack;
-            stack.push(1);
-            stack.push(5);
-            stack.push(3);
-            stack.push(2);
+
+            const int N = 6;
+            int val_arr[N] = { 40, 10, 100, 90, 20, 25 };
+
+            for (int i = 0; i < N; ++i)
+            {
+                stack.push(val_arr[i]);
+            }
 
             stack_sort<std::stack<int>>(stack);
 
-            const int N = 4;
-            int cmp_arr[N] = { 1, 2, 3, 5 };
+            qsort(val_arr, N, sizeof(int), compare);
 
             for (int i = 0; i < N; ++i)
             {
                 int top = stack.top();
                 stack.pop();
-                EXPECT_EQ(cmp_arr[i], top);
+                EXPECT_EQ(val_arr[i], top);
             }
             EXPECT_TRUE(stack.empty());
         }
