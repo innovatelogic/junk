f26a1b57dd477f8750729dc9c143d4a9d87cd62d
YurG <yura.gunko@gmail.com>
2017-02-17 21:49:31 +0200

    cpp test

--- src/ut/ut_test.cpp
+++ src/ut/ut_test.cpp
@@ -97,7 +97,7 @@ namespace cpptest
         k = y.size();
     }
 
-
+    //----------------------------------------------------------------------------------------------
     namespace class_size
     {
         class A
@@ -156,11 +156,10 @@ namespace cpptest
             std::for_each(v.begin(), v.end(), std::mem_fn(&A::f));
             std::cout << std::endl;
             std::for_each(v.begin(), v.end(), std::default_delete<A>());
-
         }
     }
     
-
+    //----------------------------------------------------------------------------------------------
     namespace init_order
     {
         class A
@@ -200,6 +199,173 @@ namespace cpptest
         TEST(CppTest, InitOrder)
         {
             B b(2);
+        }
+    }
+
+    //----------------------------------------------------------------------------------------------
+    namespace std_unique
+    {
+        bool compare(int a, int b)
+        {
+            return a % 2 == b % 2;
+        }
+
+        TEST(CppTest, StdUnique)
+        {
+            int a[] = { 3, 1, 4, 6, 1, 3 };
+            auto b = std::begin(a);
+            auto e = std::end(a);
+
+
+            e = std::unique(b, e, compare);
+            std::copy(b, e, std::ostream_iterator<int>(std::cout));
+
+            std::cout << std::endl;
+        }
+    }
+
+    //----------------------------------------------------------------------------------------------
+    namespace throw_alloc
+    {
+        class A
+        {
+        public:
+            A(int n)
+            {
+                if (0 == n) {
+                    throw std::logic_error("0");
+                }
+            }
+        };
+
+        //----------------------------------------------------------------------------------------------
+
+        TEST(CppTest, ThrowAlloc)
+        {
+            A *p0 = nullptr, *p1 = nullptr, *p2 = nullptr;
+            try {
+                p1 = new A(1);
+                p0 = new A(0);
+                p2 = new A(2);
+            }
+            catch (const std::exception &) {
+                std::cout << 3;
+            }
+
+            std::cout << ((0 != p1) ? 1 : 0);
+            std::cout << ((0 != p0) ? 1 : 0);
+            std::cout << ((0 != p2) ? 1 : 0) << std::endl;
+
+            delete p1;
+            delete p0;
+            delete p2;
+        }
+    }
+
+    //----------------------------------------------------------------------------------------------
+    namespace template_template
+    {
+        /*struct A {};
+        struct B {};
+
+        template<typename T = A>
+        struct X;
+
+        template<>
+        struct X<A> {
+            static void f() { std::cout << 1 << std::endl; }
+        };
+
+        template<>
+        struct X<B> {
+            static void f() { std::cout << 2 << std::endl; }
+        };
+
+        template< template<typename T = B> class C>
+        void g() {
+            C<>::f();
+        }*/
+
+        template <template <typename> class>
+        struct X {
+            X() { std::cout << "1"; }
+        };
+
+        template <typename>
+        struct Y {};
+
+        template <typename T>
+        using Z = Y<T>;
+
+        template <>
+        struct X<Y> {
+            X() { std::cout << "2"; }
+        };
+
+        TEST(CppTest, TemplateTemplate)
+        {
+            X<Y> x1;
+            X<Z> x2;
+        }
+    }
+
+    //----------------------------------------------------------------------------------------------
+    namespace sizeof_array
+    {
+        size_t get_size_1(int* arr)
+        {
+            return sizeof arr;
+        }
+
+        size_t get_size_2(int arr[])
+        {
+            return sizeof arr;
+        }
+
+        size_t get_size_3(int(&arr)[10])
+        {
+            return sizeof arr;
+        }
+
+        TEST(CppTest, SizeofArray)
+        {
+            int array[10];
+
+            size_t _a = sizeof(array);
+            size_t _b = get_size_1(array);
+            size_t _c = get_size_2(array);
+            size_t _d = get_size_3(array);
+        }
+    }
+
+    //----------------------------------------------------------------------------------------------
+    namespace comma_innit
+    {
+        TEST(CppTest, CommaInitInt)
+        {
+            int x;
+            x = (0, 1, 2, 3, 4, 5);
+
+            EXPECT_EQ(x, 5);
+        }
+    }
+
+    //----------------------------------------------------------------------------------------------
+    namespace switch_do_while
+    {
+        TEST(CppTest, SwitchDoWhile)
+        {
+            int n = 3;
+            int i = 0;
+
+            switch (n % 2) {
+            case 0:
+                do {
+                    ++i;
+            case 1: ++i;
+                } while (--n > 0);
+            }
+            EXPECT_EQ(i, 5);
         }
     }
 }
