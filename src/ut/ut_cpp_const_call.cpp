c56826d9d46893b38de1b871dfc64118352176a7
YurG <yura.gunko@gmail.com>
2017-02-20 13:41:42 +0200

    cpp const template function instantiate

--- src/ut/ut_cpp_const_call.cpp
+++ src/ut/ut_cpp_const_call.cpp
@@ -73,4 +73,18 @@ namespace junk
             r.p->foo();
         }
     }
+
+    namespace const_template_function_instantiate
+    {
+
+        template <class T> void f(T &i) { std::cout << 1; }
+
+        template <> void f(const int &i) { std::cout << 2; }
+
+        TEST(CppTest, ConstTemplateFunctionInstantiate)
+        {
+            int i = 42;
+            f(i);
+        }
+    }
 }
