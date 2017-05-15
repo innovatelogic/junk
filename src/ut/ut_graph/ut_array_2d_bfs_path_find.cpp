c19d2b1094815eb1cd962a3fb345c70b8e4394ea
YurG <yura.gunko@gmail.com>
2017-05-16 02:24:25 +0300

    2d bfs search

---
+++ src/ut/ut_graph/ut_array_2d_bfs_path_find.cpp
@@ -0,0 +1,27 @@
+#include "graph.h"
+#include <gtest/gtest.h>
+
+namespace junk
+{
+    namespace array_2d_bfs_path_find
+    {
+        TEST(Graph, Array2dBFS_PathFind)
+        {
+            const size_t ROWS = 7;
+            const size_t COLS = 7;
+
+            int field[ROWS][COLS] = { 
+                { 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 7 },
+            };
+
+            std::vector<std::pair<int, int>> path;
+            bool flag = search_path<ROWS, COLS>(field, ROWS, COLS, 2, 2, 7, path);
+        }
+    }
+}
--- src_graph/graph.h
+++ src_graph/graph.h
@@ -4,3 +4,4 @@
 #include "dijkstra_probe_a.h"
 #include "bellman_ford_probe_a.h"
 #include "dlist_probe_a.h"
+#include "array_2d_bfs_path_find.h"
