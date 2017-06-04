5356379cb091e2dd0208ddf09cbb3e24a74083fd
YurG <yura.gunko@gmail.com>
2017-06-05 02:01:25 +0300

    upd

--- src_bits/bits_sum.h
+++ src_bits/bits_sum.h
@@ -1,18 +1,3 @@
 #pragma once
 
 #include "defexport.h"
-
-namespace junk
-{
-    namespace bits_sum
-    {
-        class BITS_EXPORT ExportType
-        {
-            ExportType() {}
-
-            virtual ~ExportType() {}
-
-            int test() { int k = 0; k++; return k; }
-        };
-    }
-}
