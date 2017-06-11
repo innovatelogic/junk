58c076b6e250619d2128748e46dda197ab396c3c
YurG <yura.gunko@gmail.com>
2017-06-11 15:55:24 +0300

    add sort_lib

---
+++ src_sort/sort_lib.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "qsort_probe_a.h"
+#include "qsort_probe_b.h"
+#include "sort_insertion.h"
+#include "sort_insertion_probe_a.h"
+#include "sort_insertion_probe_b.h"
+#include "sort_selection_probe_a.h"
+#include "merge_sort.h"
+#include "merge_sort_probe_a.h"
+#include "sort_merge_probe_b.h"
+#include "sort_merge_probe_c.h"
---
+++ src_sort/ut/main.cpp
@@ -0,0 +1,13 @@
+//#include <iostream>
+#include <gtest/gtest.h>
+
+//#include "../inner_template_class.h"
+
+int main(int argc, char **argv) 
+{
+    //Holder<int> t;
+
+	::testing::InitGoogleTest( &argc, argv );
+	return RUN_ALL_TESTS();
+    return 0;
+}
